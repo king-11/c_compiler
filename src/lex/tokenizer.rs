127eb33595e45440e4e14ae9845e05b97ef3d9dd
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-07-27 23:01:53 +0530

    fix: tokenizer match complete word if no delimit
    separate tokens were made out of substring
    match the whole word and then tokenize
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/lex/tokenizer.rs
+++ src/lex/tokenizer.rs
@@ -11,44 +11,36 @@ pub fn recurse_tokens(value: &str) -> Vec<Token> {
   let mut end_idx = 0;
   let byte_array = value.as_bytes();
   while end_idx < byte_array.len() {
-      if value.get(end_idx..end_idx+3) == Some("int") {
-          tokens.push(Token::Int);
-          end_idx += 3;
-          continue;
-      }
+    let literal_regex = Regex::new("^\\w+").unwrap();
 
-      if value.get(end_idx..end_idx+6) == Some("return") {
-          tokens.push(Token::Return);
-          end_idx += 6;
-          continue;
-      }
+    if let Some(val) = literal_regex.find(&value[end_idx..]) {
+        let number_regex = Regex::new("^\\d+").unwrap();
+        if let Some(token_val) = Token::keywords(val.as_str()) {
+            tokens.push(token_val);
+        }
+        else if let Some(val) = number_regex.find(&value[end_idx..]) {
+            let result = val.as_str().parse().unwrap();
+            tokens.push(Token::Integer(result));
+        }
+        else {
+            tokens.push(Token::Identifier(val.as_str().to_string()));
+        }
 
-      let number_regex = Regex::new("^\\d+").unwrap();
-      if let Some(val) = number_regex.find(&value[end_idx..]) {
-          let result = val.as_str().parse().unwrap();
-          tokens.push(Token::Integer(result));
-          end_idx += val.end();
-          continue;
-      }
+        end_idx += val.end();
+        continue;
+    };
 
-      let identifier_regex = Regex::new("^[a-zA-Z]\\w*").unwrap();
-      if let Some(val) = identifier_regex.find(&value[end_idx..]) {
-          tokens.push(Token::Identifier(val.as_str().to_string()));
-          end_idx += val.end();
-          continue;
-      }
-
-      let val = &byte_array[end_idx];
-      let simple_token = match *val as char {
-          '{' => Token::OpenBrace,
-          '}' => Token::CloseBrace,
-          '(' => Token::OpenParenthesis,
-          ')' => Token::CloseParenthesis,
-          ';' => Token::SemiColon,
-          val => if val.is_whitespace() { end_idx += 1; continue } else { break }
-      };
-      end_idx += 1;
-      tokens.push(simple_token);
+    let val = &byte_array[end_idx];
+    let simple_token = match *val as char {
+        '{' => Token::OpenBrace,
+        '}' => Token::CloseBrace,
+        '(' => Token::OpenParenthesis,
+        ')' => Token::CloseParenthesis,
+        ';' => Token::SemiColon,
+        val => if val.is_whitespace() { end_idx += 1; continue } else { break }
+    };
+    end_idx += 1;
+    tokens.push(simple_token);
   }
 
   tokens

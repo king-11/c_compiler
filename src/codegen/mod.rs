5c63c82bbbe4b8a45637d7a16a9c2f8eb3a075a1
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-08-03 01:03:43 +0530

    feat: add binary op codegen
    - convert to 64bit registers
    - stack can only work with 64bit registers not %eax
    
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,4 +1,4 @@
-use crate::{ast::model::*, lex::UnaryOperator};
+use crate::{ast::model::*, lex::{UnaryOperator, BinaryOperator}};
 
 pub fn generate(root: &Program) -> String {
   generate_function(&root.func)
@@ -10,8 +10,7 @@ fn generate_function(func: &Function) -> String {
   format!(
   "\t.globl {name}
 {name}:
-{body}
-  ", name = func.name, body = body)
+{body}", name = func.name, body = body)
 }
 
 fn generate_statement(st: &Statement) -> String {
@@ -19,22 +18,34 @@ fn generate_statement(st: &Statement) -> String {
     Statement::Return(val) => {
       let exp = generate_expression(val);
 
-      format!("{exp}\n\tret")
+      format!("{exp}\n\tret\n")
     }
   }
 }
 
 fn generate_expression(exp: &Expression) -> String {
   match exp {
-    Expression::Const(val) => format!("\tmovl\t${}, %eax", val.to_string()),
+    Expression::Const(val) => format!("\tmov\t${}, %rax", val.to_string()),
     Expression::Unary { op, exp } => {
       let inner_exp = generate_expression(exp);
       let ext_exp = match op {
-        UnaryOperator::Negation => format!("neg\t%eax"),
-        UnaryOperator::BitwiseComplement => format!("not\t%eax"),
+        UnaryOperator::Negation => format!("neg\t%rax"),
+        UnaryOperator::BitwiseComplement => format!("not\t%rax"),
         UnaryOperator::LogicalNegation => {
-          format!("cmpl\t$0, %eax\n\tmovl\t$0, %eax\n\tsete\t%al")
+          format!("cmp\t$0, %rax\n\tmov\t$0, %rax\n\tsete\t%al")
         }
+      };
+      format!("{}\n\t{}", inner_exp, ext_exp)
+    },
+    Expression::Binary { exp1, op, exp2 } => {
+      let exp1 = generate_expression(exp1);
+      let exp2 = generate_expression(exp2);
+      let inner_exp = format!("{}\n\tpush\t%rax\n{}\n\tpop\t%rcx", exp1, exp2);
+      let ext_exp = match op {
+        BinaryOperator::Addition => format!("add\t%rcx, %rax"),
+        BinaryOperator::Multiplication => format!("imul\t%rcx, %rax"),
+        BinaryOperator::Minus => format!("sub\t%rax, %rcx\n\tmov\t%rcx, %rax"),
+        BinaryOperator::Division => format!("mov\t%rax, %rbx\n\tmov\t%rcx, %rax\n\tcqo\n\tidiv\t%rbx",)
       };
       format!("{}\n\t{}", inner_exp, ext_exp)
     }

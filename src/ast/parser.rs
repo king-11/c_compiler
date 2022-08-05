4d013b854bcee271de737f5f14429c5d8c593f67
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-08-05 16:52:43 +0530

    refactor: DNY parsing functions
    pass function as parameters
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -19,15 +19,16 @@ fn parse_factor(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
   }
 }
 
-fn parse_term(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
-  let mut factor = parse_factor(tokens)?;
+fn parse_sub_function(tokens: &mut Scanner, sub_exp: fn(&mut Scanner) -> Result<Expression, SyntaxError>, operators: &[BinaryOperator]) -> Result<Expression, SyntaxError> {
+  let mut exp = sub_exp(tokens)?;
 
   while let Some(val) = tokens.peek() {
     if let Ok(bin_op) = BinaryOperator::try_from(val.clone()) {
-      if let BinaryOperator::Multiplication | BinaryOperator::Division = bin_op {
-        tokens.pop("not expecting error here").unwrap();
-        let next_factor = parse_factor(tokens)?;
-        factor = Expression::Binary { exp1: Box::new(factor), op: bin_op, exp2: Box::new(next_factor) }
+      if operators.contains(&bin_op) {
+        // not expecting error here
+        tokens.pop("").unwrap();
+        let next_exp = sub_exp(tokens)?;
+        exp = Expression::Binary { exp1: Box::new(exp), op: bin_op, exp2: Box::new(next_exp) }
       }
       else {
         break;
@@ -38,117 +39,31 @@ fn parse_term(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
     }
   }
 
-  Ok(factor)
+  Ok(exp)
+}
+
+fn parse_term(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
+  parse_sub_function(tokens, parse_factor, &[BinaryOperator::Multiplication, BinaryOperator::Division])
 }
 
 fn parse_additive_expression(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
-  let mut term = parse_term(tokens)?;
-
-  while let Some(val) = tokens.peek() {
-    if let Ok(bin_op) = BinaryOperator::try_from(val.clone()) {
-      if let BinaryOperator::Addition | BinaryOperator::Minus = bin_op {
-        tokens.pop("not expection error").unwrap();
-        let next_term = parse_term(tokens)?;
-        term = Expression::Binary { exp1: Box::new(term), op: bin_op, exp2: Box::new(next_term) }
-      }
-      else {
-        break;
-      }
-    }
-    else {
-      break;
-    }
-  }
-
-  Ok(term)
+  parse_sub_function(tokens, parse_term, &[BinaryOperator::Addition, BinaryOperator::Minus])
 }
 
 fn parse_relational_expression(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
-  let mut additive_exp = parse_additive_expression(tokens)?;
-
-  while let Some(val) = tokens.peek() {
-    if let Ok(bin_op) = BinaryOperator::try_from(val.clone()) {
-      if let BinaryOperator::LessThan | BinaryOperator::LessThanOrEqual | BinaryOperator::GreaterThan | BinaryOperator::GreaterThanOrEqual = bin_op {
-        tokens.pop("not expection error").unwrap();
-        let next_additive_exp = parse_additive_expression(tokens)?;
-        additive_exp = Expression::Binary { exp1: Box::new(additive_exp), op: bin_op, exp2: Box::new(next_additive_exp) }
-      }
-      else {
-        break;
-      }
-    }
-    else {
-      break;
-    }
-  }
-
-  Ok(additive_exp)
+  parse_sub_function(tokens, parse_additive_expression, &[BinaryOperator::LessThan, BinaryOperator::LessThanOrEqual, BinaryOperator::GreaterThan, BinaryOperator::GreaterThanOrEqual])
 }
 
 fn parse_equality_expression(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
-  let mut relational_exp = parse_relational_expression(tokens)?;
-
-  while let Some(val) = tokens.peek() {
-    if let Ok(bin_op) = BinaryOperator::try_from(val.clone()) {
-      if let BinaryOperator::Equal | BinaryOperator::NotEqual = bin_op {
-        tokens.pop("not expection error").unwrap();
-        let next_relational_exp = parse_relational_expression(tokens)?;
-        relational_exp = Expression::Binary { exp1: Box::new(relational_exp), op: bin_op, exp2: Box::new(next_relational_exp) }
-      }
-      else {
-        break;
-      }
-    }
-    else {
-      break;
-    }
-  }
-
-  Ok(relational_exp)
+  parse_sub_function(tokens, parse_relational_expression, &[BinaryOperator::Equal, BinaryOperator::NotEqual])
 }
 
 fn parse_logical_and_expression(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
-  let mut equality_exp = parse_equality_expression(tokens)?;
-
-  while let Some(val) = tokens.peek() {
-    if let Ok(bin_op) = BinaryOperator::try_from(val.clone()) {
-      if BinaryOperator::And == bin_op {
-        tokens.pop("not expection error").unwrap();
-        let next_equality_exp = parse_equality_expression(tokens)?;
-        equality_exp = Expression::Binary { exp1: Box::new(equality_exp), op: bin_op, exp2: Box::new(next_equality_exp) }
-      }
-      else {
-        break;
-      }
-    }
-    else {
-      break;
-    }
-  }
-
-  Ok(equality_exp)
+  parse_sub_function(tokens, parse_equality_expression, &[BinaryOperator::And])
 }
 
 fn parse_expression(tokens: &mut Scanner) -> Result<Expression, SyntaxError> {
-  let mut logical_and_exp = parse_logical_and_expression(tokens)?;
-
-  while let Some(val) = tokens.peek() {
-    if let Ok(bin_op) = BinaryOperator::try_from(val.clone()) {
-      if BinaryOperator::Or == bin_op {
-        tokens.pop("not expection error").unwrap();
-        let next_logical_and_exp = parse_logical_and_expression(tokens)?;
-        logical_and_exp = Expression::Binary { exp1: Box::new(logical_and_exp), op: bin_op, exp2: Box::new(next_logical_and_exp) }
-      }
-      else {
-        break;
-      }
-    }
-    else {
-      break;
-    }
-  }
-
-  Ok(logical_and_exp)
+  parse_sub_function(tokens, parse_logical_and_expression, &[BinaryOperator::Or])
 }
 
 fn parse_statement(tokens: &mut Scanner) -> Result<Statement, SyntaxError> {

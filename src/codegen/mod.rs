35172c9f7fbb43ad8ca4597a1f266a6fbe407d30
Lakshya Singh <lakshay.singh1108@gmail.com>
2022-08-05 16:55:09 +0530

    feat: add in relational & logical operators
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -1,4 +1,4 @@
-use crate::{ast::model::*, lex::{UnaryOperator, BinaryOperator}};
+use crate::{ast::model::*, lex::{UnaryOperator, BinaryOperator}, utility::{generate_clause, generate_end}};
 
 pub fn generate(root: &Program) -> String {
   generate_function(&root.func)
@@ -45,7 +45,19 @@ fn generate_expression(exp: &Expression) -> String {
         BinaryOperator::Addition => format!("add\t%rcx, %rax"),
         BinaryOperator::Multiplication => format!("imul\t%rcx, %rax"),
         BinaryOperator::Minus => format!("sub\t%rax, %rcx\n\tmov\t%rcx, %rax"),
-        BinaryOperator::Division => format!("mov\t%rax, %rbx\n\tmov\t%rcx, %rax\n\tcqo\n\tidiv\t%rbx",)
+        BinaryOperator::Division => format!("mov\t%rax, %rbx\n\tmov\t%rcx, %rax\n\tcqo\n\tidiv\t%rbx"),
+        BinaryOperator::Equal => format!("cmp\t%rax, %rcx\n\tsete\t%al"),
+        BinaryOperator::NotEqual => format!("cmp\t%rax, %rcx\n\tcmp\t$0, %rax\n\tsetne\t%al"),
+        BinaryOperator::LessThan => format!("cmp\t%rax, %rcx\n\tsetl\t%al"),
+        BinaryOperator::LessThanOrEqual => format!("cmp\t%rax, %rcx\n\tsetle\t%al"),
+        BinaryOperator::GreaterThan => format!("cmp\t%rax, %rcx\n\tsetg\t%al"),
+        BinaryOperator::GreaterThanOrEqual => format!("cmp\t%rax, %rcx\n\tsetge\t%al"),
+        BinaryOperator::And => {
+          return format!("{exp1}\n\tcmp\t$0, %rax\n\tjne\t{_clause2}\n\tjmp\t{_end}\n{_clause2}:\n{exp2}\n\tcmp\t$0, %rax\n\tcmp $0, %rax\n\tsetne\t%al\n{_end}:", _clause2 = generate_clause(), _end = generate_end());
+        },
+        BinaryOperator::Or => {
+          return format!("{exp1}\n\tcmp\t$0, %rax\n\tje\t{_clause2}\n\tmov\t$1, %rax\n\tjmp\t{_end}\n{_clause2}:\n{exp2}\n\tcmp\t$0, %rax\n\tcmp $0, %rax\n\tsetne\t%al\n{_end}:", _clause2 = generate_clause(), _end = generate_end());
+        },
       };
       format!("{}\n\t{}", inner_exp, ext_exp)
     }
--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -1,4 +1,25 @@
-use std::{fmt, error::Error};
+use std::{fmt, error::Error, sync::atomic::Ordering};
+
+use std::sync::atomic::AtomicU64;
+
+static mut CLAUSE_COUNT: AtomicU64 = AtomicU64::new(0);
+static mut END_COUNT: AtomicU64 = AtomicU64::new(0);
+
+pub fn generate_clause() -> String {
+    unsafe {
+        let clause = format!("_clause{}", CLAUSE_COUNT.load(Ordering::Relaxed));
+        CLAUSE_COUNT.fetch_add(1u64, Ordering::Relaxed);
+        clause
+    }
+}
+
+pub fn generate_end() -> String {
+    unsafe {
+        let clause = format!("_end{}", END_COUNT.load(Ordering::Relaxed));
+        END_COUNT.fetch_add(1u64, Ordering::Relaxed);
+        clause
+    }
+}
 
 #[derive(Debug)]
 pub struct SyntaxError {

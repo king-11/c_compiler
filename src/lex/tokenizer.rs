fdc8509d2958790c970de083910a6f06ca3b9ad9
Lakshya Singh <lakshay.singh1108@gmail.com>
2024-08-30 14:33:15 +0530

    refactor: tokenizer modular functions
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/lex/tokenizer.rs
+++ src/lex/tokenizer.rs
@@ -1,53 +1,57 @@
 use std::rc::Rc;
 
 use regex::Regex;
+use tracing::trace;
+
+use crate::utility::SyntaxError;
 
 use super::model::Token;
 
-pub fn string_tokenizer(value: &str) -> Vec<Token> {
-  let mut tokens = Vec::new();
-  let mut end_idx = 0;
-  let byte_array = value.as_bytes();
-  while end_idx < value.len() {
+pub fn parse_literal_token(value: &str) -> (Option<Token>, usize) {
     let literal_regex = Regex::new("^\\w+").unwrap();
+    let number_regex = Regex::new("^\\d+").unwrap();
 
-    if let Some(val) = literal_regex.find(&value[end_idx..]) {
-        let number_regex = Regex::new("^\\d+").unwrap();
-        if let Some(token_val) = Token::keywords(val.as_str()) {
-            tokens.push(token_val);
-        }
-        else if let Some(val) = number_regex.find(&value[end_idx..]) {
-            let result = val.as_str().parse().unwrap();
-            tokens.push(Token::Integer(result));
-        }
-        else {
-            tokens.push(Token::Identifier(Rc::new(val.as_str().to_string())));
-        }
+    match literal_regex.find(value) {
+        Some(literal_match) => {
+            let token = if let Some(keyword) = Token::keywords(literal_match.as_str()) {
+                keyword
+            } else if let Some(number_match) = number_regex.find(literal_match.as_str()) {
+                Token::Integer(number_match.as_str().parse().expect("integer parse"))
+            } else {
+                Token::Identifier(Rc::new(literal_match.as_str().to_owned()))
+            };
 
-        end_idx += val.end();
-        continue;
+            (Some(token), literal_match.end())
+        }
+        None => (None, 0),
+    }
+}
+
+pub fn parse_compound_token(value: Option<&str>) -> (Option<Token>, usize) {
+    return match value {
+        Some(compound_value) => {
+            let compound_token = match compound_value {
+                "&&" => Token::And,
+                "||" => Token::Or,
+                "==" => Token::Equal,
+                "!=" => Token::NotEqual,
+                "<=" => Token::LessThanOrEqual,
+                ">=" => Token::GreaterThanOrEqual,
+                _ => return (None, 0),
+            };
+
+            (Some(compound_token), 2)
+        }
+        None => (None, 0),
     };
+}
 
-    let compound_val = value.get(end_idx..end_idx+2);
-    if let Some(compound_op) = compound_val {
-        let compound_token = match compound_op {
-            "&&" => Some(Token::And),
-            "||" => Some(Token::Or),
-            "==" => Some(Token::Equal),
-            "!=" => Some(Token::NotEqual),
-            "<=" => Some(Token::LessThanOrEqual),
-            ">=" => Some(Token::GreaterThanOrEqual),
-            _ => None,
-        };
-
-        if let Some(token) = compound_token {
-            end_idx += 2;
-            tokens.push(token);
-        }
+pub fn try_parse_simple_token(value: char) -> Result<(Option<Token>, usize), SyntaxError> {
+    if value.is_whitespace() {
+        return Ok((None, 1));
     }
 
-    let val = &byte_array[end_idx];
-    let simple_token = match *val as char {
+    let token = match value {
         '{' => Token::OpenBrace,
         '}' => Token::CloseBrace,
         '(' => Token::OpenParenthesis,
@@ -62,12 +66,53 @@ pub fn string_tokenizer(value: &str) -> Vec<Token> {
         '<' => Token::LessThan,
         '>' => Token::GreaterThan,
         '=' => Token::Assignment,
-        val => if val.is_whitespace() { end_idx += 1; continue } else { break }
+        val => {
+            return Err(SyntaxError::new_lex_error(format!(
+                "unidentifiable character {}",
+                val
+            )))
+        }
     };
 
-    end_idx += 1;
-    tokens.push(simple_token);
-  }
+    return Ok((Some(token), 1));
+}
 
-  tokens
+pub fn string_tokenizer(value: &str) -> Result<Vec<Token>, SyntaxError> {
+    let mut tokens = Vec::new();
+    let mut end_idx = 0;
+    let byte_array = value.as_bytes();
+    while end_idx < value.len() {
+        if let (Some(literal_value), increment) = parse_literal_token(&value[end_idx..]) {
+            trace!(
+                "literal match for token {} end index is now {}",
+                literal_value,
+                end_idx + increment
+            );
+            tokens.push(literal_value);
+            end_idx += increment;
+        } else if let (Some(compound_value), increment) =
+            parse_compound_token(value.get(end_idx..end_idx + 2))
+        {
+            trace!(
+                "compound match for token {} end index is now {}",
+                compound_value,
+                end_idx
+            );
+            tokens.push(compound_value);
+            end_idx += increment;
+        } else {
+            let (simple_token, increment) = try_parse_simple_token(byte_array[end_idx] as char)?;
+            trace!(
+                "simple match for token {:?} end index is now {}",
+                simple_token,
+                end_idx
+            );
+            if let Some(simple_value) = simple_token {
+                tokens.push(simple_value);
+            }
+            end_idx += increment;
+        }
+    }
+
+    Ok(tokens)
 }

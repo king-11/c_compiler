82e95b663209cf56c403e5c68f86a77434672e78
Lakshya Singh <lakshay.singh1108@gmail.com>
2024-08-30 14:34:30 +0530

    compiler: tracing log and error handling
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,57 @@
-use std::{io::{Error, BufReader, BufRead, Write}, fs::File, env, path::Path, process::Command};
+use std::{env, fs::File, io::{BufRead, BufReader, Write}, path::Path, process::Command};
 
 mod lex;
 
 mod ast;
 use ast::Scanner;
-use itertools::multipeek;
+use itertools::{multipeek, Itertools};
 
 mod codegen;
 use codegen::CodeGenerator;
+use tracing::{debug, info};
+use utility::SyntaxError;
 
 mod utility;
 
-fn lex(path: &str) -> Result<Vec<lex::Token>, Error> {
-    let f = File::open(path)?;
+fn lex(path: &str) -> Result<Vec<lex::Token>, SyntaxError> {
+    let f = File::open(path).expect("unable to open file");
     let f = BufReader::new(f);
     let mut token_vector = Vec::new();
     for line in f.lines() {
         let line = line.unwrap();
-        let mut tokens = lex::string_tokenizer(&line);
+        debug!("lexing line {}", line);
+        let mut tokens = lex::string_tokenizer(&line)?;
         token_vector.append(&mut tokens);
     };
 
     Ok(token_vector)
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn main() -> Result<(), SyntaxError> {
+    tracing_subscriber::fmt::fmt().init();
+
     let default_filename = "./data/stage_5/valid/exp_return_val.c".to_string();
-    let path = Path::new(args.get(1).unwrap_or(&default_filename));
+    info!("default file path {}", default_filename);
 
-    let path_value = path.to_str().unwrap();
-    match lex(path_value) {
-        Ok(tokens) => {
-            let mut scanner = Scanner::new(multipeek(tokens.iter()));
-            match ast::parse_program(&mut scanner) {
-                Ok(program) => {
-                    // println!("{}", program);
-                    let mut codegenerator = CodeGenerator::new();
-                    match codegenerator.generate(&program) {
-                        Ok(assembly) => {
-                            let filename = path.file_stem().unwrap().to_str().unwrap();
-                            let mut file = File::create(path.with_file_name(format!("{}.s", filename))).unwrap();
-                            file.write_all(assembly.as_bytes()).unwrap();
+    let path_value = env::args().get(1..2).next().unwrap_or(default_filename);
+    let path = Path::new(&path_value);
+    info!("running compiler for file {}", path_value);
 
-                            Command::new("gcc")
-                                .arg(path.with_file_name(format!("{}.s", filename)))
-                                .arg("-o")
-                                .arg(path.with_file_name(format!("{}", filename)))
-                                .output()
-                                .expect("assembly to elf failed");
-                        },
-                        Err(e) => println!("{}", e)
-                    }
-                },
-                Err(e) => {
-                    println!("{}", e);
-                }
-            }
-        },
-        Err(e) => {
-            println!("{}", e);
-        }
-    }
+    let tokens = lex(&path_value)?;
+    let mut scanner = Scanner::new(multipeek(tokens.iter()));
+    let program = ast::parse_program(&mut scanner)?;
+    let mut codegenerator = CodeGenerator::new();
+    let assembly = codegenerator.generate(&program)?;
+    let filename = path.file_stem().unwrap().to_str().unwrap();
+    let mut file = File::create(path.with_file_name(format!("{}.s", filename))).unwrap();
+    file.write_all(assembly.as_bytes()).unwrap();
+
+    Command::new("gcc")
+        .arg(path.with_file_name(format!("{}.s", filename)))
+        .arg("-o")
+        .arg(path.with_file_name(format!("{}", filename)))
+        .output()
+        .expect("assembly to elf failed");
+
+    Ok(())
 }
